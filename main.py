07a96ef5b6bbdcb9fc2b72ef9a56a7be4dc7984b
Khashayar Khosrosourmi <ksourmi@gmail.com>
2025-07-14 05:20:30 +0300

    api docs name and description and version edit

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-api = FastAPI()
+api = FastAPI(
+    title="PromptShield API",
+    description="An intelligent firewall for LLM prompts using rules and AI moderation.",
+    version="0.0.1",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json"
+)
 
 @api.post('/check', response_model=PromptResponse)
 def check_prompt(payload:PromptRequest):
